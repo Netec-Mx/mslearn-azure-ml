95ed31069bd95f849f738d258688b95b970bb299
Soporte Netec <154626442+NetecGk@users.noreply.github.com>
2025-07-08 13:53:43 -0600

    Update setup.sh

--- Labs/05/setup.sh
+++ Labs/05/setup.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/sh
+#!/usr/bin/sh
 
 # Create random string
 guid=$(cat /proc/sys/kernel/random/uuid)
@@ -7,30 +7,28 @@ suffix=${suffix:0:18}
 
 # Set the necessary variables
 RESOURCE_GROUP="rg-dp100-l${suffix}"
-RESOURCE_PROVIDER="Microsoft.MachineLearningServices"
-REGIONS=("eastus" "westus" "centralus" "northeurope" "westeurope")
-RANDOM_REGION=${REGIONS[$RANDOM % ${#REGIONS[@]}]}
+REGION="eastus"
 WORKSPACE_NAME="mlw-dp100-l${suffix}"
+COMPUTE_INSTANCE="ci${suffix}"
 COMPUTE_CLUSTER="aml-cluster"
 
-# Register the Azure Machine Learning resource provider in the subscription
-echo "Register the Machine Learning resource provider:"
-az provider register --namespace $RESOURCE_PROVIDER
 
 # Create the resource group and workspace and set to default
 echo "Create a resource group and set as default:"
-az group create --name $RESOURCE_GROUP --location $RANDOM_REGION
+az group create --name $RESOURCE_GROUP --location $REGION
 az configure --defaults group=$RESOURCE_GROUP
 
+
 echo "Create an Azure Machine Learning workspace:"
-az ml workspace create --name $WORKSPACE_NAME 
+az ml workspace create --name $WORKSPACE_NAME --location $REGION
 az configure --defaults workspace=$WORKSPACE_NAME 
 
 # Create compute cluster
 echo "Creating a compute cluster with name: " $COMPUTE_CLUSTER
-az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute 
+az ml compute create --name ${COMPUTE_CLUSTER} --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute --location $REGION
 
-# Create data asset
+
+# Create data asseta
 echo "Creating a data asset with name: diabetes-folder"
 az ml data create --name diabetes-folder --path ./data 
 
